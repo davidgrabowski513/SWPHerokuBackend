ab266b6f47374608daa030495609b002286bcb4c
davidgrabowski513 <david.gabowski@yandex.com>
2016-12-16 15:51:52 +0100

    test

--- index.js
+++ index.js
@@ -28,15 +28,15 @@ var api = new ParseServer({
   },
   push: {
     ios: [{
-      pdx: __dirname + '/cert/Certificates.p12', // the path and filename to the .p12 file you exported earlier.
+      pdx: '/cert/Certificates_123.p12', // the path and filename to the .p12 file you exported earlier.
       bundleId: 'com.boydlee.sportworldpassport', // The bundle identifier associated with your app
       production: true
     },{
-      pdx: __dirname + '/cert/swp_dev_apns.p12', // the path and filename to the .p12 file you exported earlier.
+      pdx: '/cert/swp_dev_apns_123.p12', // the path and filename to the .p12 file you exported earlier.
       bundleId: 'com.boydlee.sportworldpassport', // The bundle identifier associated with your app
       production: false
     },{
-      pdx: __dirname + '/cert/sportsWorld_apn_160419.p12', // the path and filename to the .p12 file you exported earlier.
+      pdx: '/cert/sportsWorld_apn_160419_123.p12', // the path and filename to the .p12 file you exported earlier.
       bundleId: 'com.uwp.SportWorldPassport1', // The bundle identifier associated with your app
       production: false
     }]
