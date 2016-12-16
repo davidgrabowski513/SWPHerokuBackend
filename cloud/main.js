365a9b8090b6bd3e54793f949b3ea2de36731359
davidgrabowski513 <david.gabowski@yandex.com>
2016-12-16 13:42:00 +0100

    1

--- cloud/main.js
+++ cloud/main.js
@@ -9,6 +9,7 @@ Parse.Cloud.define("Push", function(request, response) {
   var params = request.params;
   var userId = request.user;
 
+  console.log(userId);
   // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
 //  var messageText = params.alert;
 
